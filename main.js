9adb49683307a5ec6acccbd529c22a087ef6f14e
James Ryan <31721023+JamesRyan97@users.noreply.github.com>
2022-01-05 20:19:34 +0700

    Update main.js

--- main.js
+++ main.js
@@ -330,10 +330,10 @@ function getData() {
                 <td>?</td>
                 <td><div class="loader"/></td>
             </tr>`
-                tableBody = $("table tbody");
-                tableBody.append(col);
-            
-            getFullData(lineNo,code+"" + tokenID ,categoryID,categoryName,tokenID);
+            tableBody = $("table tbody");
+            tableBody.append(col);
+
+            getFullData(lineNo, code + "" + tokenID, categoryID, categoryName, tokenID);
             lineNo++;
         }
     }
@@ -346,30 +346,30 @@ function getCurrentTime() {
     return date + ' ' + time;
 }
 
-function getWholesaleCount(data, startAt, endAt){
+function getWholesaleCount(data, startAt, endAt) {
     var count = 0;
     var prices = []
     for (let item of data['list']) {
         var c = item['count'];
-        if(c >= startAt && c <= endAt){
+        if (c >= startAt && c <= endAt) {
             count++;
             prices.push(Number(item['fixed_price']) / Number(item['count']));
         }
 
-        if(item['name'] =="Metamon Egg" && item['count'] > 500 && startAt == 501){
+        if (item['name'] == "Metamon Egg" && item['count'] > 500 && startAt == 501) {
             console.log(item)
         }
     }
 
     //Sắp xếp giá trị tăng dần
-    prices.sort(function(a, b) {return a - b;});
+    prices.sort(function(a, b) { return a - b; });
 
     lowest_price = 0;
-    if(prices.length > 0)
+    if (prices.length > 0)
         lowest_price = prices[0];
     return {
-        "count" : count,
-        "lowest_price" : lowest_price
+        "count": count,
+        "lowest_price": lowest_price
     };
 }
 
@@ -384,26 +384,26 @@ async function getFullData(no, code, categoryID, categoryName, tokenId) {
             for (let item of data['list']) {
                 count += item['count'];
                 var tmpPrice = Number(item['fixed_price']) / Number(item['count'])
-                if(tmpPrice < price)
+                if (tmpPrice < price)
                     price = tmpPrice;
             }
         }
 
         //Số lượng bán lẻ 1
-        var retail_cout = getWholesaleCount(data,1,1);
-        
+        var retail_cout = getWholesaleCount(data, 1, 1);
+
         //Số lượng sỉ 2
-        var wholesale_count_2 = getWholesaleCount(data,2,50);
+        var wholesale_count_2 = getWholesaleCount(data, 2, 50);
 
         //Số lượng sỉ 51
-        var wholesale_count_51 = getWholesaleCount(data,51,150);
+        var wholesale_count_51 = getWholesaleCount(data, 51, 150);
 
         //Số lượng sỉ 151
-        var wholesale_count_151 = getWholesaleCount(data,151,500);
+        var wholesale_count_151 = getWholesaleCount(data, 151, 500);
 
         //Số lượng sỉ 500
-        var wholesale_count_500 = getWholesaleCount(data,501, 999999999999);
-      
+        var wholesale_count_500 = getWholesaleCount(data, 501, 999999999999);
+
         var col = `<tr>
                         <th scope="row">${no}</th>
                         <td>${categoryName}</td>
@@ -419,13 +419,13 @@ async function getFullData(no, code, categoryID, categoryName, tokenId) {
                         </td>
                     </tr>`;
         document.getElementById(code).innerHTML = col
-        document.getElementById('timeUpdate').innerHTML = "Cập nhật lần cuối: " + getCurrentTime();
+        document.getElementById('timeUpdate').innerHTML = "Cập nhật lần cuối: " + getCurrentTime() + ' | Auth: <a href="https://t.me/batrong2709">Dev James</a>';
     });
 }
 
-function getValueCol(val, isRetail = false){
+function getValueCol(val, isRetail = false) {
     var color = isRetail ? 'rgb(248, 100, 2)' : 'rgb(219, 2, 248)'
-    if(val['count'] == 0)
+    if (val['count'] == 0)
         return `<td>x</td>`;
     else
         return `<td><span style="color: ${color}">${Intl.NumberFormat().format(val['count'])}</span><br><span style="color: rgb(2, 109, 248);">${Intl.NumberFormat().format(val['lowest_price'])}</span> <img src="./raca.png" width="15px" height="15px"/></td>`;
